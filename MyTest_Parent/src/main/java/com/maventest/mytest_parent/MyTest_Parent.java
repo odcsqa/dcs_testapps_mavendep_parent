a968ec57358984c0200b00a344e47c917ecd9342
clara.coder <devservice.alex@gmail.com>
2016-03-01 08:37:31 -0700

    Add the files

---
+++ MyTest_Parent/src/main/java/com/maventest/mytest_parent/MyTest_Parent.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.maventest.mytest_parent;
+
+
+/**
+ *
+ * @author nvrishab
+ */
+public class MyTest_Parent {
+    
+    public static void main(String[] args) {
+        sayHello();
+      //  MyTest.sayHello();
+    }
+    
+    public static void sayHello() {
+        System.out.println ("Hello From MyTest_Parent !");
+    }
+}
